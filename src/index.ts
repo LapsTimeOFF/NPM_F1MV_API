471ba2a0b1c6f2139c86dbd19d6ddc33e85e6c2f
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-12-30 00:17:23 +0100

    Adding all imports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import { Config } from './Types';
+import { Config, Topic } from './Types';
 import { getF1MVVersion, getAPIVersion } from './getVersion';
 import { discoverF1MVInstances } from './discoverF1MVInstances';
-import { LiveTimingAPIV1 } from './apiCall';
+import { LiveTimingAPIV1, LiveTimingAPIV2 } from './apiCall';
 import { testConnection } from './connection';
+import { noInstanceFounded, invalidTopic } from './Errors';
 
-export { Config, getAPIVersion, getF1MVVersion, discoverF1MVInstances, LiveTimingAPIV1, testConnection };
+export {
+    Config,
+    getAPIVersion,
+    getF1MVVersion,
+    discoverF1MVInstances,
+    LiveTimingAPIV1,
+    testConnection,
+    Topic,
+    LiveTimingAPIV2,
+    noInstanceFounded,
+    invalidTopic,
+};
