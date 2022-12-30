1602838dc4028b6b0eb1c41c453b5747fe06f84a
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2022-12-30 14:07:38 +0100

    Checking if the test connection is really an F1MV and not a random software

--- src/connection.ts
+++ src/connection.ts
@@ -3,7 +3,10 @@ import { Config } from "./Types";
 export async function testConnection(config: Config) {
     try {
         const res = await (await fetch(`http://${config.host}:${config.port}/api/v1/app/version`)).json();
-        return res;
+        if(res.version !== undefined) {
+            return res;
+        }
+        return false;
     } catch (error) {
         return false;
     }
