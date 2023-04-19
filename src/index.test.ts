e1216df05ae4f87437f5d46d550d611527aca51a
LapsTimeOFF <39061148+LapsTimeOFF@users.noreply.github.com>
2023-04-19 17:45:42 +0200

    update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,26 +1,26 @@
-import * as functions from './index';
+import * as index from './index';
+import * as connection from './connection';
+import * as discoverF1MVInstances from './discoverF1MVInstances';
+import * as Errors from './Errors';
+import * as GraphQL from './GraphQL';
+import * as LiveTimingAPI from './LiveTimingAPI';
+import * as Player from './Player';
+import * as publicAPI from './publicAPI';
+import * as Types from './Types';
+import * as getVersion from './getVersion';
 
-describe('index', () => {
-  it('exports the correct functions and variables and types', () => {
-    expect(functions.getAPIVersion).toBeDefined();
-    expect(functions.getFIA_Documents).toBeDefined();
-    expect(functions.getCircuitInfo).toBeDefined();
-    expect(functions.getPlayerBounds).toBeDefined();
-    expect(functions.removePlayer).toBeDefined();
-    expect(functions.syncPlayers).toBeDefined();
-    expect(functions.setPlayerBounds).toBeDefined();
-    expect(functions.createPlayer).toBeDefined();
-    expect(functions.setSpeedometerVisibility).toBeDefined();
-    expect(functions.getAllPlayers).toBeDefined();
-    expect(functions.customGraphQL).toBeDefined();
-    expect(functions.getF1MVVersion).toBeDefined();
-    expect(functions.discoverF1MVInstances).toBeDefined();
-    expect(functions.LiveTimingAPIV1).toBeDefined();
-    expect(functions.LiveTimingAPIV2).toBeDefined();
-    expect(functions.LiveTimingAPIGraphQL).toBeDefined();
-    expect(functions.LiveTimingClockAPIGraphQL).toBeDefined();
-    expect(functions.testConnection).toBeDefined();
-    expect(functions.noInstanceFounded).toBeDefined();
-    expect(functions.invalidTopic).toBeDefined();
+describe('verify exports', () => {
+  test('exports all', () => {
+    expect(index).toEqual({
+      ...connection,
+      ...discoverF1MVInstances,
+      ...Errors,
+      ...GraphQL,
+      ...LiveTimingAPI,
+      ...Player,
+      ...publicAPI,
+      ...Types,
+      ...getVersion,
+    });
   });
 });
